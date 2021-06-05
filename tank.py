18039b98b088f88952c2a033403ae136757b4178
Matt Hummer <matt.hummer@nutrien.com>
2021-06-05 15:04:51 -0600

    stub out updater and handlers

--- tank.py
+++ tank.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from threading import Thread
+from time import sleep
 
 class Tank:
   class Pins(Enum):
@@ -9,10 +11,18 @@ class Tank:
     LED_HEAD=None
 
   def __init__(self):
-    pass
+    self.is_firing = False
+    # self.update_thread = self._initialize_update_thread()
 
   def handle_movement(self, data):
-    print('======== move: ', data)
+    axis = data['axis']
+    handler = {
+      "X": lambda: self._handle_turning(data),
+      "Y": lambda: self._handle_acceleration(data),
+      "Z": lambda: self._handle_turret_rotation(data),
+      "THROTTLE": lambda: self._handle_throttle(data),
+    }.get(str(axis)) or (lambda: None)
+    handler()
 
   def handle_press(self, button):
     print('======== handle_press: ', button)
@@ -23,15 +33,38 @@ class Tank:
 
   def handle_release(self, button):
     print('======== handle_release: ', button)
+    handler = {
+      "0": lambda: self._ceasefire_turrets(),
+    }.get(str(button)) or (lambda: None)
+    handler()
 
-  def _handle_turning(self):
-    pass
+  def _handle_turning(self, data):
+    print('======== turn: ', data)
 
-  def _handle_acceleration(self):
-    pass
+  def _handle_acceleration(self, data):
+    print('======== accelerate: ', data)
 
-  def _handle_turret_rotation(self):
-    pass
+  def _handle_turret_rotation(self, data):
+    print('======== rotate: ', data)
+
+  def _handle_throttle(self, data):
+    print('======== throttle: ', data)
 
   def _fire_turrets(self):
     print("fire!")
+    self.is_firing = True
+
+  def _ceasefire_turrets(self):
+    self.is_firing = False
+
+  def _initialize_update_thread(self):
+    def set_pins_from_state():
+      while True:
+        sleep(.15) # Throttle this bad boy so it's not running a bajillion FPS
+        # TODO: flicker turret LED if firing
+        # TODO: rotate turret mount servo if rotation delta
+    t = Thread(target=set_pins_from_state)
+    t.start()
+
+    return t
+    
