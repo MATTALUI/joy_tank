5c08495800d55828c40a61df01fbe4e31115b80d
Matthew Hummer <biggreyson@gmail.com>
2021-06-06 17:39:17 -0600

    add basic turret rotation

--- Tank.py
+++ Tank.py
@@ -7,18 +7,22 @@ class Tank:
   class Pins(IntEnum):
     #WHEEL_RIGHT=None
     #WHEEL_LEFT=None
+    SERVO_TURRET=27
     LED_TURRET=17
     #LED_DRIVE=None
     #LED_HEAD=None
 
   def __init__(self):
-    self.is_firing = False
+    self.turret_delta = 0.0
+
     self.turret_led = LED(self.Pins.LED_TURRET)
+    self.turret_mount = Servo(self.Pins.SERVO_TURRET, min_pulse_width=0.75/1000, max_pulse_width=2.25/1000, frame_width=20/1000)
+    self.turret_mount.value = 0.0
     # self.head_led = LED(self.Pins.LED_HEAD)
     # self.drive_led = LED(self.Pins.LED_DRIVE)
     # self.left_wheel = Servo(self.Pins.WHEEL_LEFT)
     # self.left_right = Servo(self.Pins.WHEEL_RIGHT)
-    # self.update_thread = self._initialize_update_thread()
+    self.update_thread = self._initialize_update_thread()
 
   def handle_movement(self, data):
     axis = data['axis']
@@ -63,21 +67,22 @@ class Tank:
       pass
 
   def _handle_turret_rotation(self, data):
-    print('======== rotate: ', data)
+    power = data['power']
+    new_delta = 0
+    if power == 'LOW':
+        new_delta = 0.01
+    elif power == 'HIGH':
+        new_delta = 0.05
     if data['direction'] == '+':
       # TODO: Rotate CW
-      pass
-    else:
-      # TODO: Rotate CCW
-      pass
+      new_delta = -new_delta
+    self.turret_delta = new_delta
 
   def _handle_throttle(self, data):
     print('======== throttle: ', data)
     # TODO: Investigate wether or not we can even do something interesting with the throttle
 
   def _fire_turrets(self):
-    print("fire!")
-    self.is_firing = True
     self.turret_led.blink(on_time=1/15, off_time=1/12)
 
   def _ceasefire_turrets(self):
@@ -87,9 +92,11 @@ class Tank:
   def _initialize_update_thread(self):
     def set_pins_from_state():
       while True:
-        sleep(.15) # Throttle this bad boy so it's not running a bajillion FPS
-        # TODO: flicker turret LED if firing
-        # TODO: rotate turret mount servo if rotation delta
+        sleep(.01) # Throttle this bad boy so it's not running a bajillion FPS
+        next_value = self.turret_mount.value + self.turret_delta
+        if (next_value < 1.0) and (next_value > -1.0):
+            self.turret_value = next_value
+            self.turret_mount.value += self.turret_delta
     t = Thread(target=set_pins_from_state)
     t.start()
 
---
+++ start.sh
@@ -0,0 +1,2 @@
+sudo pigpiod
+GPIOZERO_PIN_FACTORY=pigpio python3 receiver.py
